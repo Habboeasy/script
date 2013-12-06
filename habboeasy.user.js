8241c377505fe92a3a8b5a9aec077c5c7b96cb9f
Habboeasy <enderbit@live.com>
2013-12-06 19:28:19 +0100

    Create habboeasy.user.js

---
+++ habboeasy.user.js
@@ -0,0 +1,60 @@
+// MyHabboPlus Userscript (http://www.MyHabboPlus.com) are under GPLv2: https://www.gnu.org/licenses/gpl-2.0.html
+// ==UserScript==
+// @name        Habboeasy
+// @namespace   http://23445.id1945.com
+// @description Userscript for Habbo Hotel
+// @version     1.0.1
+// @author		Xander
+// @author		Vlacox
+
+// @icon        http://i.imgur.com/4ZnViub.png
+// @include     http://www.habbo.es/*
+// @include     https://www.habbo.es/*
+// @include     http://www.habbo.com/*
+// @include     https://www.habbo.com/*
+// @include     http://www.habbo.fi/*
+// @include     https://www.habbo.fi/*
+// @include     http://www.habbo.com.br/*
+// @include     https://www.habbo.com.br/*
+// @include     http://www.habbo.com.tr/*
+// @include     https://www.habbo.com.tr/*
+// @include     http://www.habbo.it/*
+// @include     https://www.habbo.it/*
+// @include     http://www.habbo.fr/*
+// @include     https://www.habbo.fr/*
+// @include     http://www.habbo.nl/*
+// @include     https://www.habbo.nl/*
+// @include     http://www.habbo.de/*
+// @include     https://www.habbo.de/*
+// @include     http://www.habbo.dk/*
+// @include     https://www.habbo.dk/*
+// @include     http://www.habbo.no/*
+// @include     https://www.habbo.no/*
+// @include     http://www.habbo.se/*
+// @include     https://www.habbo.se/*
+
+// @exclude     http://www.habbo.*/
+// @exclude     http://www.habbo.*/account/submit
+// @exclude     http://www.habbo.*/identity/*
+// @exclude     https://www.habbo.*/
+// @exclude     https://www.habbo.*/account/submit
+// @exclude     https://www.habbo.*/identity/*
+
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_addStyle
+// @grant       GM_getResourceText
+// @grant       GM_getResourceURL
+// @grant       GM_xmlhttpRequest
+// @resource    CSSnewhabbo https://raw.github.com/Habboeasy/script/newhabbo.css
+// @resource    CSSmyhabboplus https://raw.github.com/Habboeasy/script/myhabboplus.css
+// @resource    PNGSprites http://i.imgur.com/MhilY5Q.png
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+// @require     https://raw.github.com/Habboeasy/script/config.js
+// @require     https://raw.github.com/Habboeasy/script/langs.js
+// @require     https://raw.github.com/Habboeasy/script/system.js
+// @require     https://raw.github.com/Habboeasy/script/updater.js
+// @require     https://raw.github.com/Habboeasy/script/habboHome.js
+// @require     https://raw.github.com/Habboeasy/script/habboSearch.js
+// @require     https://raw.github.com/Habboeasy/script/newArticles.js
+// ==/UserScript==
